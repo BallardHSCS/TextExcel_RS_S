c5b010cf0c689e0c2518864a4121feddff07c7c0
Straitjacket Roland <31781296+bigr0@users.noreply.github.com>
2018-03-19 14:46:52 -0700

    Additions Plz no merge cobflicts xddd o fug
    Added some editions for check 2 and 3, we still need to build our board using a double for loop and changing characters (described in psuedocode)

---
+++ ExcelSpreasheet/src/SpreadSheet.java
@@ -0,0 +1,14 @@
+public class SpreadSheet {
+    public static final int SHEET_HEIGHT = 10;
+    public static final int SHEET_WIDTH = 7;
+    String[][] cellArray = new String[SHEET_WIDTH][SHEET_HEIGHT];
+
+    public SpreadSheet(){}
+
+    public void rebuildSheet(){}
+
+
+    public
+
+
+}
