033df0213e6d7c8ef467f1f58854698dfbc3371d
svenmnnilsson <31781163+svenmnnilsson@users.noreply.github.com>
2018-03-26 15:15:43 -0700

    fixed bitch

--- ExcelSpreasheet/src/SpreadSheet.java
+++ ExcelSpreasheet/src/SpreadSheet.java
@@ -1,18 +1,62 @@
 public class SpreadSheet {
     public static final int SHEET_HEIGHT = 10;
     public static final int SHEET_WIDTH = 7;
-    String[][] cellArray = new String[SHEET_WIDTH][SHEET_HEIGHT];
 
-    public SpreadSheet(){
+    Cell[][] cellArray = new Cell[SHEET_WIDTH][SHEET_HEIGHT];
+
+    public SpreadSheet() {
+        for (int wPos = 0; wPos <= SHEET_WIDTH; wPos++) {
+
+
+            for (int hPos = 0; hPos <= SHEET_HEIGHT; hPos++) {
+                cellArray[wPos][hPos] = new Cell();
+
+
+            }
+        }
     }
 
-    public void printEmptySpreadsheet(){
+    public void SpreadSheetPrint() {
+        for (int i = 0; i <= SHEET_HEIGHT/2; i++) {
+            if (i % 0 != 0) {
+                System.out.println("------------+------------+------------+------------+------------+------------+------------+");
+            }
+            if (i % 0 == 0) {
+                System.out.println("|            |            |            |            |            |            |            |");
+
+            }
+
+
+
+
+        }
+    }
+
+public static void main(String args[]){
+        
+
+
+
+
+
+
 
 
     }
-
-
-    public
-
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
