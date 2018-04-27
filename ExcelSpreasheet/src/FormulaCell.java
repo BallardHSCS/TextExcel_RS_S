219b0e5ff16efa1f565e2063f6b7eebf1847ae2c
svenmnnilsson <31781163+svenmnnilsson@users.noreply.github.com>
2018-04-27 14:52:33 -0700

    Formula layout
    made empty layout for formula cell

--- ExcelSpreasheet/src/FormulaCell.java
+++ ExcelSpreasheet/src/FormulaCell.java
@@ -10,4 +10,12 @@ public class FormulaCell extends Cell {
     public void formulaCell() {
 
     }
+public double Average(){
+        return 1.0;
+}
+
+public double Sum(){
+    return 1.0;
+    }
+
 }
