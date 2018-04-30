b531d442c252a6f45b50552415a99f8eee536af9
svenmnnilsson <31781163+svenmnnilsson@users.noreply.github.com>
2018-04-30 15:12:31 -0700

    updated gitignore
    Revoed a bunch of  .xml files

--- ExcelSpreasheet/src/FormulaCell.java
+++ ExcelSpreasheet/src/FormulaCell.java
@@ -6,12 +6,39 @@
 * */
 
 
-public class FormulaCell extends Cell {
+import java.util.ArrayList;
 
+public class FormulaCell extends Cell {
+ArrayList <Cell> refCells = new ArrayList<Cell>();
 
     /*TODO create a formula method that is called after a formula is interpreted in parsing
       TODO
     */
     public void formulaCell() {
+
+
+
     }
+    public void Average(){
+
+    }
+    public void sum(){
+        int total = 0 ;
+        for(int i = 0; i<refCells.length(); i++){
+            total += new Integer(refCells.get(i));
+        }
+
+    }
+    public void order(){
+
+    }
+    public void reverseOrder(){
+
+    }
+    public void sorter(){
+
+
+    }
+
+
 }
