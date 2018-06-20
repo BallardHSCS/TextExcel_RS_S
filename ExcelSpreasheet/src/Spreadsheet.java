b32a74ea3f73ac5103602508933f1cc9644df5f4
Straitjacket Roland <31781296+bigr0@users.noreply.github.com>
2018-06-20 09:43:04 -0700

    Sorting
    Sort A and D

--- ExcelSpreasheet/src/Spreadsheet.java
+++ ExcelSpreasheet/src/Spreadsheet.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 import static oracle.jrockit.jfr.events.Bits.doubleValue;
 
 public class Spreadsheet {
@@ -25,10 +27,11 @@ public class Spreadsheet {
     }
 
     /**
-     * @todo check data going into the toString. Any nulls?
      * @return prints out the entire sheet
+     * @todo check data going into the toString. Any nulls?
      */
-   /*ERROR*/ public String toString() {
+   /*ERROR*/
+    public String toString() {
         String sheetPrint = "";
         String nextRow = "------------+------------+------------+------------+------------+------------+------------+------------+\n";
         sheetPrint += "            |     A      |     B      |     C      |     D      |     E      |     F      |     G      |\n" + nextRow;
@@ -40,7 +43,8 @@ public class Spreadsheet {
             }
             for (int cPos = 0; cPos < SHEET_WIDTH; cPos++) {
 
-/*ERROR*/                sheetPrint += cellArray[rPos][cPos].printToSpreadsheet();
+/*ERROR*/
+                sheetPrint += cellArray[rPos][cPos].printToSpreadsheet();
                 sheetPrint += "|";
 
             }
@@ -53,45 +57,43 @@ public class Spreadsheet {
     }
 
     /**
-     *
-     * @param type
-     * clears the sheet.
+     * @param type clears the sheet.
      */
 
     public void clear(String type) {
         if (type.contains(" ")) {
-/**/            if(type.contains("-")){
+/**/
+            if (type.contains("-")) {
                 String[] range_parts = type.split(" ");
                 String firstCell = range_parts[1];
                 char letter = Character.toUpperCase(firstCell.charAt(0));
-                int row = Integer.parseInt(firstCell.substring(1))-1;
+                int row = Integer.parseInt(firstCell.substring(1)) - 1;
                 int col = letter - letterA;
 
 
                 String secondCell = range_parts[3];
                 char letterTwo = Character.toUpperCase(secondCell.charAt(0));
-                int rowTwo = Integer.parseInt(secondCell.substring(1))-1;
+                int rowTwo = Integer.parseInt(secondCell.substring(1)) - 1;
                 int colTwo = letterTwo - letterA;
 
-                for(int c = col; c <= colTwo + 1 ; c ++){
-                    for(int r = row; r <= rowTwo; r ++){
+                for (int c = col; c <= colTwo + 1; c++) {
+                    for (int r = row; r <= rowTwo; r++) {
                         cellArray[row][col] = new Cell();
                     }
-      /**/          }
+      /**/
+                }
 
 
+            } else {
+                String[] user_input_parts = type.split(" ");
+                String cellName = user_input_parts[1];
+                char letter = cellName.charAt(0);
+                letter = Character.toUpperCase(letter);
+                int row = Integer.parseInt(cellName.substring(1)) - 1;
+                int col = letter - letterA;
+                cellArray[row][col] = new Cell();
             }
-            else{
-            String[] user_input_parts = type.split(" ");
-            String cellName = user_input_parts[1];
-            char letter = cellName.charAt(0);
-            letter = Character.toUpperCase(letter);
-            int row = Integer.parseInt(cellName.substring(1))-1;
-            int col = letter - letterA;
-            cellArray[row][col] = new Cell();
-        }}
-
-        else {
+        } else {
             for (int let = 0; let < SHEET_HEIGHT; let++) {
                 for (int num = 0; num < SHEET_WIDTH; num++) {
                     cellArray[let][num] = new Cell();
@@ -102,16 +104,15 @@ public class Spreadsheet {
     }
 
     /**
-     *
      * @param input
      * @return print out the cell, int the form of a string
      */
 
-    public String printCell(String input){
+    public String printCell(String input) {
 
         char letter = input.charAt(0);
         letter = Character.toUpperCase(letter);
-        int row = Integer.parseInt(input.substring(1))-1;
+        int row = Integer.parseInt(input.substring(1)) - 1;
         int col = letter - letterA;
         String cellprint = cellArray[row][col].returnToSpreadsheet();
         return cellprint;
@@ -127,23 +128,22 @@ public class Spreadsheet {
         char stringChecker = '"';
         FormulaCell formulaCell = new FormulaCell(Cell.EMPTY_CELL, this);
         NumberCell number;
-        if(input.contains("(")){
+        if (input.contains("(")) {
 
-                String data = "";
-                String[] user_input_parts = input.split(" ");
-                String cellName = user_input_parts[0];
-                char letter = cellName.charAt(0);
-                letter = Character.toUpperCase(letter);
-                int row = Integer.parseInt(cellName.substring(1)) - 1;
-                int col = letter - letterA;
-                for (int i = 2; i <= user_input_parts.length - 1; i++) {
-                    data += user_input_parts[i] + " ";
-                }
-                cellArray[row][col] = new FormulaCell(data, this);
-
-
+            String data = "";
+            String[] user_input_parts = input.split(" ");
+            String cellName = user_input_parts[0];
+            char letter = cellName.charAt(0);
+            letter = Character.toUpperCase(letter);
+            int row = Integer.parseInt(cellName.substring(1)) - 1;
+            int col = letter - letterA;
+            for (int i = 2; i <= user_input_parts.length - 1; i++) {
+                data += user_input_parts[i] + " ";
             }
-        else if(input.contains(stringChecker + "")){
+            cellArray[row][col] = new FormulaCell(data, this);
+
+
+        } else if (input.contains(stringChecker + "")) {
             String[] user_input_parts = input.split(" ");
             String cellName = user_input_parts[0];
             String data = user_input_parts[2];
@@ -152,9 +152,7 @@ public class Spreadsheet {
             int row = Integer.parseInt(cellName.substring(1)) - 1;
             int col = letter - letterA;
             cellArray[row][col] = new Cell(data);
-        }
-
-        else {
+        } else {
             String[] user_input_parts = input.split(" ");
             String cellName = user_input_parts[0];
             String data = user_input_parts[2];
@@ -165,32 +163,64 @@ public class Spreadsheet {
             cellArray[row][col] = new NumberCell(data);
 
 
-        }}
-
-    public  String getCell(int r, int c){
-        return  cellArray[r][c].printToSpreadsheet();
+        }
     }
 
-public void order() {
-}
-
-    public void reverseOrder() {
+    public String getCell(int r, int c) {
+        return cellArray[r][c].printToSpreadsheet();
     }
 
-    public void sorter(int[] a) {
+
+    public void sorter(String input) {
+        int size = 0;
+        int inc = 0;
+        String[] parseForCells = input.split(" ");
+        String firstCell = parseForCells[1];
+        char letter = firstCell.charAt(0);
+        letter = Character.toUpperCase(letter);
+        int row = Integer.parseInt(firstCell.substring(1)) - 1;
+        int col = letter - letterA;
+
+
+        String secondCell = parseForCells[3];
+        char letterTwo = secondCell.charAt(0);
+        letterTwo = Character.toUpperCase(letterTwo);
+        int rowTwo = Integer.parseInt(secondCell.substring(1)) - 1;
+        int colTwo = letterTwo - letterA;
+
+        for (int r = row; r <= rowTwo; r++) {
+            for (int c = col; c <= colTwo; c++) {
+               size++;
+
+            }
+        }
+
+        double[] dubs = new double [size];
+
+        for(int r2 = row; r2 <= rowTwo; r2++){
+            for(int c2 = col; c2 <= colTwo; c2++){
+                dubs[inc] = Double.parseDouble(cellArray[r2][c2].printToSpreadsheet());
+                inc++;
+            }
+        }
+
+        sortA(dubs);
+
+    }
+
+    public static void sortA(double[] a) {
         for (int i = 0; i < a.length; i++) {
             int minPos = minimumPosition(a, i);
             swap(a, minPos, i);
         }
-    }
 
+    }
     /**
-     *
      * @param a
      * @param from
      * @return returns the minimum position
      */
-    private static int minimumPosition(int a[], int from) {
+    private static int minimumPosition(double a[], int from) {
         int minPos = from;
         for (int i = from + 1; i < a.length; i++) {
             if (a[i] < a[minPos]) {
@@ -200,7 +230,7 @@ public void order() {
         return minPos;
     }
 
-    public static int linearSearch(int[] a, int x) {
+    public static int linearSearch(double[] a, int x) {
         for (int i = 0; i < a.length; i++) {
             if (a[i] == x) {
                 return i;
@@ -210,8 +240,8 @@ public void order() {
 
     }
 
-    private static void swap(int a[], int i, int j) {
-        int temp = a[i];
+    private static void swap(double a[], int i, int j) {
+        double temp = a[i];
         a[i] = a[j];
         a[j] = temp;
     }
